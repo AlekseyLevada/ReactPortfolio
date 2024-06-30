2f6da6d14063d6a4fffb9a01fb80c12185a0a98c
Aleksey Levada <aleksey.levada@gmail.com>
2024-06-30 12:57:27 +0300

    Внесены правки в роутинг

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { RouterProvider } from "react-router-dom"
+import { BrowserRouter, RouterProvider } from "react-router-dom"
 import { Router } from "./utils/router/Router"
 
 export const App = () => {
 
   return (
     <div className="_customer-app">
-    <RouterProvider router={Router}/>
+    <RouterProvider router={Router} history={BrowserRouter} basename="/ReactPortfolio"/>
     </div>
   )
 }
