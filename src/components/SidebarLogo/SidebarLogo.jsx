15250144513d1e97be11421340ec5aca4c47746f
Aleksey Levada <aleksey.levada@gmail.com>
2024-06-30 07:23:39 +0300

    Добавлены медиазапросы к Main Layout

--- src/components/SidebarLogo/SidebarLogo.jsx
+++ src/components/SidebarLogo/SidebarLogo.jsx
@@ -1,4 +1,4 @@
-import './styles/style.css'
+import './styles/style.scss'
 import { Link } from 'react-router-dom'
 
 export const SidebarLogo = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.jsx'
 import './styles/reset.scss'
-import './styles/mediaQueries.css'
+import './styles/mediaQueries.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
