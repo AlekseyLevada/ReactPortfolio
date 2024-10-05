fb1f8425262d120cc90c6f6edaf06c33dcde9159
Aleksey Levada <aleksey.levada@gmail.com>
2024-10-05 20:46:25 +0300

    Удалены неиспользуемые пакеты

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, RouterProvider } from "react-router-dom"
 import { Router } from "./utils/router/Router"
 
-
 export const App = () => {
-
   return (
-    <main className="_customer-app">
+    <main className="_customer-app" id="_customerApp">
     <RouterProvider router={Router} history={BrowserRouter} basename="/ReactPortfolio"/>
     </main>
   )
