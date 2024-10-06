68fa1bec3f363df021575765d5258a7e415221dc
Aleksey Levada <aleksey.levada@gmail.com>
2024-10-06 14:04:16 +0300

    Правки меню

--- src/lib/menuItems.js
+++ src/lib/menuItems.js
@@ -3,20 +3,24 @@ export const menuItems = [
     name: "Главная",
     link: "/",
   },
+
   {
     name: "О себе",
     link: "/about"
   },
+
   {
     name: "Мои работы",
     link: "/portfolio"
   },
-  {
-    name: "Отзывы",
-    link: "/reviews"
-  },
-  {
-    name: "Контакты",
-    link: "/contacts"
-  }
+
+  // {
+  //   name: "Отзывы",
+  //   link: "/reviews"
+  // },
+
+  // {
+  //   name: "Контакты",
+  //   link: "/contacts"
+  // }
 ]
