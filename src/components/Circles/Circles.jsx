0191b283e21febc82b969805c286f419828caa51
Aleksey Levada <aleksey.levada@gmail.com>
2025-07-18 17:53:14 +0300

    Refresh

--- src/components/Circles/Circles.jsx
+++ src/components/Circles/Circles.jsx
@@ -4,7 +4,7 @@ import mainPhoto from '../../img/int_img.webp'
 export const Circles = () => {
   return (
     <div className="_customer-circles">
-              <img src={mainPhoto} alt="" loading="lazy" width="520" height="520"/>
+              <img src={mainPhoto} alt="" loading="lazy"/>
     </div>
   )
 }
--- src/components/PortfolioWorkCard/PortfolioWorkCard.jsx
+++ src/components/PortfolioWorkCard/PortfolioWorkCard.jsx
@@ -9,7 +9,7 @@ export const PortfolioWorkCard = (props) => {
           {props.title}
         </div>
         <div className="_customer-portfolio-work-card-preview">
-          <img src={props.preview} alt="" draggable="false"loading='lazy' width="389" height="235"/>
+          <img src={props.preview} alt={`Preview of ${props.title}`} draggable="false"loading='lazy'/>
         </div>
         <div className="_customer-portfolio-work-card-skills">
           {props.skills}
