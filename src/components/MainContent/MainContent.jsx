f322153afaa6a6ab7a9bd0ebf9e2a85601493f9c
Aleksey Levada <aleksey.levada@gmail.com>
2025-07-25 19:30:58 +0300

    Добавлено фоновое видео

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -5,8 +5,20 @@ import { AppBtn } from '../AppBtn/AppBtn'
 export const MainContent = () => {
   return (
     <section className='_customer-main-content'>
+      <div className="video-background">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src="http://localhost:5173/ReactPortfolio/videos/bg_video.mp4" type="video/mp4" />
+          <source src="http://localhost:5173/ReactPortfolio/videos/bg_video.webm" type="video/webm" />
+          Ваш браузер не поддерживает видео.
+        </video>
+      </div>
       <IntroText />
-      <AppBtn link="/about" text="Вперед"></AppBtn>
+      <AppBtn link="/about" text="Вперед" />
     </section>
   )
 }
