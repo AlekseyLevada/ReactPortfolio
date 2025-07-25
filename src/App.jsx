86c224362d48767eb7ad309c523730f666739fa4
Aleksey Levada <aleksey.levada@gmail.com>
2025-07-25 18:22:37 +0300

    Небольшой редизайн убраны Particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, RouterProvider } from "react-router-dom"
 import { Router } from "./utils/router/Router"
-import ParticlesComponent from "./components/Particles/Particles"
 
 export const App = () => {
   return (
     <main className="_customer-app" id="_customerApp">
-      <ParticlesComponent id="particles"/>
     <RouterProvider router={Router} history={BrowserRouter} basename="/ReactPortfolio"/>
     </main>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/ReactPortfolio",
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  }
 })
