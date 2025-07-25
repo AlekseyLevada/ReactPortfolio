6810dee9d7718e5b49dac1e721badb5de1b3622b
Aleksey Levada <aleksey.levada@gmail.com>
2025-07-25 16:20:02 +0300

    Адаптивность изображений

--- src/components/PortfolioWorkCard/PortfolioWorkCard.jsx
+++ src/components/PortfolioWorkCard/PortfolioWorkCard.jsx
@@ -9,7 +9,7 @@ export const PortfolioWorkCard = (props) => {
           {props.title}
         </div>
         <div className="_customer-portfolio-work-card-preview">
-          <img src={props.preview} alt={`Preview of ${props.title}`} draggable="false"loading='lazy'/>
+          <img src={props.preview} alt={`Preview of ${props.title}`} draggable="false"loading='lazy' width="385" height="232"/>
         </div>
         <div className="_customer-portfolio-work-card-skills">
           {props.skills}
