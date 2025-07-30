d795cbefd02120d45e3ddc9a45fd6b90ea54d312
Aleksey Levada <aleksey.levada@gmail.com>
2025-07-30 16:29:26 +0300

    Добавлена работа финского спортивного магазина

--- src/lib/portfolioWorks.js
+++ src/lib/portfolioWorks.js
@@ -1,110 +1,83 @@
-import preview_1 from '../img/portfolio_peviews/preview_1.avif'
-import preview_2 from '../img/portfolio_peviews/preview_2.avif'
-import preview_3 from '../img/portfolio_peviews/preview_3.avif'
-import preview_4 from '../img/portfolio_peviews/preview_4.avif'
-import preview_5 from '../img/portfolio_peviews/preview_5.avif'
-import preview_6 from '../img/portfolio_peviews/preview_6.avif'
-import preview_7 from '../img/portfolio_peviews/preview_7.avif'
-import preview_8 from '../img/portfolio_peviews/preview_8.avif'
-import preview_9 from '../img/portfolio_peviews/preview_9.avif'
-import preview_10 from '../img/portfolio_peviews/preview_10.avif'
-import preview_11 from '../img/portfolio_peviews/preview_11.avif'
-import preview_12 from '../img/portfolio_peviews/preview_12.avif'
+import portfolio_preview from '../img/portfolio_peviews/preview_portfolio.avif'
+import konstruct_preview from '../img/portfolio_peviews/preview_konstruct.avif'
+import js_to_do_preview from '../img/portfolio_peviews/preview_js_to_do.avif'
+import react_to_do_preview from '../img/portfolio_peviews/preview_react_to_do.avif'
+import krovelnaya_preview from '../img/portfolio_peviews/preview_krovelnaya.avif'
+import darstroy_preview from '../img/portfolio_peviews/preview_darstroy.avif'
+import akum_preview from '../img/portfolio_peviews/preview_akum.avif'
+import skala_preview from '../img/portfolio_peviews/preview_skala.avif'
+import preview_xxl from '../img/portfolio_peviews/preview_xxl.avif'
 
 export const portfolioWorks = [
   {
-    id: 1,
+    id: "399d59ab-4d2c-4d01-8188-5d85e641b709",
     workTitle: "Portfolio",
-    workPreview: preview_1,
+    workPreview: portfolio_preview,
     workSkills: "html css react swiper",
     workLink: "https://alekseylevada.github.io/ReactPortfolio/"
   },
 
   {
-    id: 2,
+    id: "d6d4526d-5647-4bdb-a2ae-ae393d94813e",
     workTitle: "Konstruct",
-    workPreview: preview_2,
+    workPreview: konstruct_preview,
     workSkills: "html css js",
     workLink: "https://alekseylevada.github.io/Konstruct/"
   },
 
   {
-    id: 3,
-    workTitle: "Кровельная Компания",
-    workPreview: preview_3,
-    workSkills: "html css js swiper php",
-    workLink: "https://www.kfk174.ru/"
-  },
-
-  {
-    id: 4,
-    workTitle: "Дарстрой",
-    workPreview: preview_4,
-    workSkills: "html css js swiper php",
-    workLink: "https://www.xn--74-6kcp0a3anaik.xn--p1ai/"
-  },
-
-  {
-    id: 5,
-    workTitle: "Аккумуляторная компания",
-    workPreview: preview_5,
-    workSkills: "html css js swiper php",
-    workLink: "https://www.akb98-shop.ru/"
-  },
-
-  {
-    id: 6,
-    workTitle: "Компания СКАЛА",
-    workPreview: preview_6,
-    workSkills: "html css js",
-    workLink: "https://www.jbi-16.ru/"
-  },
-
-  {
-    id: 7,
-    workTitle: "amoCRM Test Task",
-    workPreview: preview_7,
-    workSkills: "html css js",
-    workLink: "https://alekseylevada.github.io/amoCRM-count-down/"
-  },
-
-  {
-    id: 8,
-    workTitle: "REST API",
-    workPreview: preview_8,
-    workSkills: "node.js Expess.js mysql",
-    workLink: "https://github.com/AlekseyLevada/Node.js-express-server"
-  },
-
-  {
-    id: 9,
-    workTitle: "Todo List",
-    workPreview: preview_9,
+    id: "6ce22a5a-dc6d-47e9-9fd1-ecdb29ea8b42",
+    workTitle: "JS Todo List",
+    workPreview: js_to_do_preview,
     workSkills: "Нативный JavaScript",
     workLink: "https://alekseylevada.github.io/JavaScript-To-Do-List/"
   },
 
   {
-    id: 10,
+    id: "774786b4-1220-442c-83d6-0ec8afff2b80",
     workTitle: "React To Do List",
-    workPreview: preview_10,
+    workPreview: react_to_do_preview,
     workSkills: "React TS Vite",
     workLink: "https://alekseylevada.github.io/To-Do_List-TS-React-Vite-/"
   },
 
   {
-    id: 11,
-    workTitle: "NDM Test Task",
-    workPreview: preview_11,
-    workSkills: "React TS RxJs ip-Address",
-    workLink: "https://alekseylevada.github.io/NDM-Test-Task/"
+    id: "44628e72-8e84-43c5-8904-15b2d77a4f4e",
+    workTitle: "Кровельная Компания",
+    workPreview: krovelnaya_preview,
+    workSkills: "html css js swiper php",
+    workLink: "https://www.kfk174.ru/"
   },
 
   {
-    id: 12,
-    workTitle: "TensorFlow Sidebar",
-    workPreview: preview_12,
-    workSkills: "React Vite Styled-Componets",
-    workLink: "https://alekseylevada.github.io/Frontend-HTML-task/"
+    id: "eb538e4e-b682-4e29-8ed5-5410788a8d6b",
+    workTitle: "Дарстрой",
+    workPreview: darstroy_preview,
+    workSkills: "html css js swiper php",
+    workLink: "https://www.xn--74-6kcp0a3anaik.xn--p1ai/"
   },
+
+  {
+    id: "663f9296-6ee4-4ed3-bc1e-6c974df96d4b",
+    workTitle: "Аккумуляторная компания",
+    workPreview: akum_preview,
+    workSkills: "html css js swiper php",
+    workLink: "https://www.akb98-shop.ru/"
+  },
+
+  {
+    id: "c651f717-c1cf-4d3b-b71d-8fb2a77af292",
+    workTitle: "Компания СКАЛА",
+    workPreview: skala_preview,
+    workSkills: "html css js",
+    workLink: "https://www.jbi-16.ru/"
+  },
+
+  {
+    id: "c651f717-c1cd-4d2b-b71d-8xb2a77at292",
+    workTitle: "Финский спортивный магазин",
+    workPreview: preview_xxl,
+    workSkills: "React CSS Modules Vite",
+    workLink: "https://www.xxl.fi/"
+  }
 ]
